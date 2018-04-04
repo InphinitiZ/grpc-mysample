4373f91f4404e93541199b7e0afba5a78c8ad05c
海尽 <jinshi.zjs@alibaba-inc.com>
2018-04-04 15:09:06 +0800

    init

---
+++ src/main/java/Application.java
@@ -0,0 +1,32 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA
+ * User: jinshi.zjs
+ * Date: 2018/4/4
+ * Time: 14:55
+ */
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/now")
+    String hehe() {
+        return "现在时间：" + (new Date()).toLocaleString();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
